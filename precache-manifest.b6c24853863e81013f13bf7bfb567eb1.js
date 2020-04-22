ef42ff4665a6ffe795e3c0cca59813c082f3d2cd
ymx1997 <1169212079@qq.com>
2020-04-22 22:28:14 +0800

    Updates

---
+++ precache-manifest.b6c24853863e81013f13bf7bfb567eb1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8c4ca0e6fe1793c0cec16242bf5634",
+    "url": "/wnzf/index.html"
+  },
+  {
+    "revision": "ed92b9aa8e3c9756b704",
+    "url": "/wnzf/static/css/2.38aff980.chunk.css"
+  },
+  {
+    "revision": "71a810235f641386e674",
+    "url": "/wnzf/static/css/main.d20dc4e0.chunk.css"
+  },
+  {
+    "revision": "ed92b9aa8e3c9756b704",
+    "url": "/wnzf/static/js/2.5e7b4db7.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/wnzf/static/js/2.5e7b4db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a810235f641386e674",
+    "url": "/wnzf/static/js/main.5c6947b3.chunk.js"
+  },
+  {
+    "revision": "cb86ce73fb420f9bde85",
+    "url": "/wnzf/static/js/runtime-main.32d6dd30.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/wnzf/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/wnzf/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/wnzf/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/wnzf/static/media/iconfont.f58b610e.woff"
+  }
+]);
