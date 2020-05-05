f2ae4102e60979668a22a82fabe27e3f66ed3bb9
ymx1997 <1169212079@qq.com>
2020-05-05 11:43:36 +0800

    地图找房-创建了html覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -2,9 +2,9 @@
  * 地图找房
  */
 import React, { Component } from 'react'
-import './index.scss'
+import styles from './index.module.css'
 import { NavBar, Icon } from 'antd-mobile'
-
+import { getCurCity } from '../../utils/quanju'
 
 class Map extends Component {
 
@@ -13,21 +13,61 @@ class Map extends Component {
     }
 
     // 初始化地图
-    initMap = () => {
+    initMap = async () => {
         const { BMap } = window;
         // 打断点
         // console.log(BMap);
         // 创建地图实例
         const map = new BMap.Map("container");
         // 地图定位的经纬度设置(天安门)
-        let point = new BMap.Point(116.404, 39.915);
-        // 设置地图的位置和缩放级别
-        map.centerAndZoom(point, 15);
+        // let point = new BMap.Point(116.404, 39.915);
+        // // 设置地图的位置和缩放级别
+        // map.centerAndZoom(point, 15);
+        // 获取当前定位城市的信息
+        let { value, label } = await getCurCity();
+        // 创建地址解析器实例
+        let myGeo = new BMap.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野
+        myGeo.getPoint(null, function (point) {
+            if (point) {
+                map.centerAndZoom(point, 11);
+                // map.addOverlay(new BMap.Marker(point));
+                // 添加空间 =》 平移缩放控件
+                map.addControl(new BMap.NavigationControl());
+                // 比例尺
+                map.addControl(new BMap.ScaleControl());
+
+                // 创建文本覆盖物
+                // 覆盖物参数
+                let opts = {
+                    position : point, // 指定文本标注所在的地理位置
+                    offset : new BMap.Size(0, 0) // 设置文本偏移量
+                }
+                // 初始化覆盖物实例
+                let label = new BMap.Label(null, opts); // 创建文本标注对象
+                // 使用html创建覆盖物
+                label.setContent(
+                    `
+                    <div class="${styles.bubble}">
+                    <p class="${styles.bubbleName}">天安门</p>
+                    <p>1888套</p>
+                    </div>`
+                )
+                // 设置覆盖物的样式
+                label.setStyle({
+                    background: 'transparent',
+                    border: 0
+                });
+                // 调用百度地图的实例提供的addOverlay方法 =》添加覆盖物到地图
+                map.addOverlay(label);
+            }
+        },
+        label)
     }
 
     render() {
         return (
-            <div className="xBai">
+            <div className={styles.mapBox}>
                 {/* 导航栏 */}
                 <NavBar
                     mode="dark"
